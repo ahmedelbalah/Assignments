40b9ef10ae26775f156e66c19f4f551ae38d16b0
Ahmed Elbalah <ahmedelbalah24@gmail.com>
2025-08-19 16:38:52 +0300

    Examination System

---
+++ Assiments/Exam.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examination_System
+{
+    internal class Exam
+    {
+        private String Question;
+        private String Result;
+        private String namecourse;
+        public String question { get { return Question;  } set { Question = value;  } }
+        public String result { get { return Result; } set { Result = value; } }
+
+        
+    }
+}
---
+++ Assiments/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examination_System
+{
+    internal class Student
+    {
+
+        private int Id;
+        private string Name;
+        private String Email;
+
+        public Student(int id , String n , String e) 
+        {
+        this.Id = id;
+        this.Name = n;
+        this.Email = e;
+        }
+
+        public int id { get { return Id; } set { Id = value; } }
+        public string name { get { return Name; } set { Name = value; } }
+        public String email { get { return Email; } set { Email = value; } }
+
+
+    }
+}
